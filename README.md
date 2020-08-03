d93243262f37c7288601c89dc1379e8445f457c3
SendyMederos <sendymederos@hotmail.com>
2020-08-03 15:55:46 -0400

    Made some of the code a function that calls itself if the criteria isn't accomplished/ Deleted and arranged variables to improve effectiveness/ created a README

---
+++ README.md
@@ -0,0 +1,23 @@
+
+Password Generator
+
+
+        On this application we have generated a random password based on user-selected criteria. 
+    This app will run in the browser and feature dynamically updated HTML  powered by JavaScript code. 
+    It features a responsive interface.
+
+
+On this application: 
+    * When the user click the button to generate a password the user is presented with a series of prompts for password criteria:
+        > The length of the password. This length must fall between 8 and 128. 
+        > The character types to include in the password, where the user could choose:
+            lowercase characters, 
+            uppercase characters, 
+            numeric characters, and/or 
+            special characters.
+            At least one character type must be selected.    
+    * At least one of each selected characters type will be included on the random password. 
+    * The password will be written to the page.
+
+
+
--- script.js
+++ script.js
@@ -1,70 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//declaring the variables 
-var characterSelector = {
- lowerCase : "abcdefghijklmnopqrstuvwxyz",
- upperCase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
- numbers : '0123456789',
- specialCh : '!@#$%^&*()_:"<>?/.,;-{}[]~',
- 
-};
 
-// creating function to generate the password
+
+
+// This function was created to select the type of characters and to call itself if no type of characters were selected 
+function typeOfCharacters () {
+    //declaring the variables 
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = '0123456789';
+    var specialCh = '!@#$%^&*()_:"<>?/.,;-{}[]~';
+    
+    // Asking if the the password should contain lower case, upper case, numbers, and/or special characters. 
+    if (confirm (" Would you like your password to contain lower case characters?")) {
+            //passing values to a variable that will contain them all
+        allbut += lowerCase;
+            //ensuring that ther is at least one of this characters on the password
+        passwordOnProgress += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+            //this counter will be substracted from the length selected to keep the length of the password
+        counter ++;
+    };
+
+    if (confirm (" Would you like your password to contain upper case characters?" )) {
+          //passing values to a variable that will contain them all
+        allbut += upperCase;
+            //ensuring that ther is at least one of this characters on the password
+        passwordOnProgress += upperCase[Math.floor(Math.random() * upperCase.length)];
+            //this counter will be substracted from the length selected to keep the length of the password
+        counter ++;
+    };
+
+    if (confirm (" Would you like your password to contain numbers?" )) {
+          //passing values to a variable that will contain them all
+        allbut += numbers;
+            //ensuring that ther is at least one of this characters on the password
+        passwordOnProgress += numbers[Math.floor(Math.random() * numbers.length)];
+            //this counter will be substracted from the length selected to keep the length of the password
+        counter ++;
+    };
+
+    if (confirm (" Would you like your password to contain special characters?" )) {
+        //passing values to a variable that will contain them all
+        allbut += specialCh;
+          //ensuring that ther is at least one of this characters on the password
+        passwordOnProgress += specialCh[Math.floor(Math.random() * specialCh.length)];
+          //this counter will be substracted from the length selected to keep the length of the password
+        counter ++;
+    }; 
+    // if no character types were selected then called 
+  if ( allbut === "") {
+        alert ( "At least one character type has to be selected" );
+        typeOfCharacters();
+      };
+    };
+
+
+//  function to generate the password
 function generatePassword(){
-
   let passwordLength = 0;
-  var allbut = "";
-  let passwordOnProgress = "";
-  let counter = 0;
+  allbut = "";
+  passwordOnProgress = "";
+  counter = 0;
   // Prompt for the password lenght until the number is between 8 and 128.
   while ( passwordLength < 8 || passwordLength > 128) {
   alert ("Please select a number between 8 and 128" );
-  passwordLength = prompt("How long do you want the pasword to be?");
+  passwordLength = prompt("How long do you want the password to be?");
  };
 
-  // Asking if the the pasword should contain lower case, upper case, numbers, and/or special characters. 
-  if (lowerCaseQuest = confirm (" Would you like your pasword to contain lower case?")) {
-          //passing values to a variable that will contain them all
-    allbut += characterSelector.lowerCase;
-        //ensuring that ther is at least one of this characters on the password
-    passwordOnProgress += characterSelector.lowerCase[Math.floor(Math.random() * characterSelector.lowerCase.length)];
-        //this counter will be substracted from the length selected to keep the length of the password
-    counter ++;
-  };
-
-  if (upperlCaseQuest = confirm (" Would you like your pasword to contain upper case?" )) {
-        //passing values to a variable that will contain them all
-    allbut += characterSelector.upperCase;
-        //ensuring that ther is at least one of this characters on the password
-    passwordOnProgress += characterSelector.upperCase[Math.floor(Math.random() * characterSelector.upperCase.length)];
-        //this counter will be substracted from the length selected to keep the length of the password
-    counter ++;
-  };
-
-  if (numbersQuest = confirm (" Would you like your pasword to contain numbers?" )) {
-        //passing values to a variable that will contain them all
-    allbut += characterSelector.numbers;
-        //ensuring that ther is at least one of this characters on the password
-    passwordOnProgress += characterSelector.numbers[Math.floor(Math.random() * characterSelector.numbers.length)];
-        //this counter will be substracted from the length selected to keep the length of the password
-    counter ++;
-  };
-
-  if (specialChQuest = confirm (" Would you like your pasword to contain special characters?" )) {
-       //passing values to a variable that will contain them all
-    allbut += characterSelector.specialCh;
-       //ensuring that ther is at least one of this characters on the password
-    passwordOnProgress += characterSelector.specialCh[Math.floor(Math.random() * characterSelector.specialCh.length)];
-       //this counter will be substracted from the length selected to keep the length of the password
-    counter ++;
-
-  }; 
-
-    // looping to get a random letters  for the lenght selected minus the counter.
+  //calling the function that will ask what type of characters should the passwrod contain
+  typeOfCharacters();
+    
+    // looping to select as many random letters as the lenght selected minus the counter.
   for ( i = 0; i < passwordLength - counter; i++ ) {
     passwordOnProgress += allbut[Math.floor(Math.random() * allbut.length)];
   };
-
 
   // switching the the string into an array
   let fromStringToArray = passwordOnProgress.split("");
@@ -82,8 +91,9 @@ function generatePassword(){
   };
     //changing back the array to a string 
   passwordOnProgress = fromStringToArray.join("");
-     //return the final password 
+     //return the final password
   return passwordOnProgress;
+
 };
 
 
